10ebff5d79883e06d616d63ae338b5cafe793b1d
DARSHAN <darshanhelode@gmail.com>
2025-11-30 17:42:39 +0530

    Revert "added new changes"
    This reverts commit ccc450ffa345f3eaa1c7ef01beb8cb3b83d701ec.

--- src/main/java/com/darshan/controller/DashBoardController.java
+++ src/main/java/com/darshan/controller/DashBoardController.java
@@ -5,8 +5,4 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class DashBoardController {
 
-    public static void main(String[] args) {
-        System.out.println("This msg i want to revert ");
-    }
-
 }
