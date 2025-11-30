adae096a07529f0a60db4195159981526cb8792e
DARSHAN <darshanhelode@gmail.com>
2025-11-30 17:51:30 +0530

    Revert "revert this"
    This reverts commit 0bd642b61a6884c17eef2ebebc58714c4eb13823.

--- src/main/java/com/darshan/controller/DashBoardController.java
+++ src/main/java/com/darshan/controller/DashBoardController.java
@@ -1,13 +1,8 @@
 package com.darshan.controller;
 
-import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class DashBoardController {
-
-    public static void main(String[] args) {
-        System.out.println("DARSHAN");
-    }
 
 }
